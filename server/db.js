25df95548a64ecae828eeed7babf39099373a557
Kevin Defalco <kevindefalco@gmail.com>
2022-11-04 17:11:51 -0300

    updating db for deploy

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 
 import { createPool } from "mysql2/promise";
+import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from "./config.js";
 
 export const pool = new createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'faztpassword',
-    database: 'taskd'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 })
 
